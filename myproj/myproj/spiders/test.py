bae9b225cc2627e1a5973fa6488aa22eb619b6e8
pjebz <pauljasper99@gmail.com>
2018-03-02 08:09:50 -0600

    use dictionary instead of Item class

--- myproj/myproj/spiders/test.py
+++ myproj/myproj/spiders/test.py
@@ -1,4 +1,5 @@
 from scrapy.spiders import Spider
+from scrapy.http import Request
 #from scrapy.selector import HtmlXPathSelector
 from scrapy.selector import Selector
 from myproj.items import  MainList, SubList
@@ -156,3 +157,47 @@ class MySpider5(Spider):
             wordlist.append(w)
         return wordlist
 
+
+
+class MySpider5(Spider):
+    '''
+    scrape the titles and the subtitles
+    '''
+    name = "words1"
+    allowed_domains = ['manythings.org']
+    start_urls = ['http://www.manythings.org/vocabulary/lists/c/']
+
+    def parse(self, response):
+        #hxs = Selector(response)
+        mains = response.xpath("//div//ul/li")
+        print(">>> ITEM INFO")
+        L = []
+        for index, main in enumerate(mains):
+            mainlist = {}
+            main_title = main.xpath('b/text()').extract()
+            try:
+                mainlist['title'] = main_title[0]
+                sublist = []
+                for subtitle in main.xpath('.//ul/li'):
+                    asub = {}
+                    sub_title = subtitle.xpath('a/text()').extract()
+                    sub_url = subtitle.xpath('a/@href').extract()
+                    try:
+                        asub['title'] = sub_title[0]
+                        try:
+                            asub['url'] = 'http://www.manythings.org/vocabulary/lists/c/' + sub_url[0]
+                        except:
+                            asub['url'] = ''
+                        sublist.append(asub)
+                    except:
+                        pass
+                mainlist['sublist'] = sublist
+                L.append(mainlist)
+            except:
+                pass         
+        for item in L:
+            print(item)
+        return L
+
+    def parseWords(self, url):
+        return ['foo']
